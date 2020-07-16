0988bbf1cc8d55817ae8b37d42b60af4ddd575ce
sidaga1 <sidaga@uw.edu>
2020-07-16 00:37:06 -0700

    absoultely cannot figure out how to actually transfer data between view controllers

---
+++ puzzles/VeiwControllers/CategoryViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CategoryViewController.swift
+//  puzzles
+//
+//  Created by Sidharth Daga on 7/15/20.
+//  Copyright © 2020 Sidharth Daga. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+
+    @IBOutlet weak var nBar: UINavigationItem!
+    @IBOutlet weak var tableView: UITableView!
+    var category: String = ""
+    var endorsements: [String: Dictionary<String, Array<Any>>] = [:]
+    var endorse = [Int: [String: Array<Any>]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        nBar.title = category
+        print(category)
+        print(endorsements)
+        var x = 0
+        let v = endorsements[category]
+        for endorsem in v! {
+            var temp = [String: Array<Any>]()
+            temp[endorsem.key] = endorsem.value
+            endorse[x] = temp
+            x = x + 1
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        endorsements.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var ans = ""
+        for key in endorse[indexPath.row]!.keys {
+            ans = key
+        }
+        cell.textLabel?.text = ans
+        return cell
+       }
+       
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- puzzles/VeiwControllers/EndorsementsViewController.swift
+++ puzzles/VeiwControllers/EndorsementsViewController.swift
@@ -10,23 +10,28 @@ import UIKit
 import FirebaseFirestore
 import FirebaseAuth
 import Firebase
-class EndorsementsViewController: UIViewController{
-
+class EndorsementsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
 
+    @IBOutlet weak var navBar: UINavigationItem!
+    @IBOutlet weak var tblView: UITableView!
     @IBOutlet weak var firstNAME: UILabel!
-    
-    @IBOutlet weak var displayNAME: UILabel!
-    
     @IBOutlet weak var lastNAME: UILabel!
     
+    var cat = ""
     var endoses = [String: Dictionary<String, Array<Any>>]()
     var endorsCollectionRef: CollectionReference!
     var profileCollectionRef: CollectionReference!
+    var userEndos = ["Movies", "Books", "TV Shows", "Hikes", "Restaurants", "Artists"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+        print(userEndos)
+        print("--------------------")
         endorsCollectionRef = Firestore.firestore().collection("users").document(Auth.auth().currentUser?.email as! String).collection("categories")
         endorsCollectionRef.getDocuments { (snap, error) in
                   if error != nil {
@@ -34,34 +39,6 @@ class EndorsementsViewController: UIViewController{
                   } else {
                       for document in snap!.documents {
                           self.endoses[document.documentID] = document.data() as? Dictionary
-                      }
-                    let cent = self.view.center
-                    let xcor = cent.x
-                    var ycor = cent.y - 100
-                      for map in self.endoses {
-                        let category = UILabel()
-                        var times = 0
-                        category.text = map.key
-                        category.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
-                        category.center = CGPoint(x: xcor, y: ycor)
-                        category.backgroundColor = UIColor.yellow
-                        category.textAlignment = .center
-                        self.view.addSubview(category)
-                        let names = UILabel()
-                        names.text = ""
-                        for cat in map.value {
-                            names.text! += cat.key + "\n"
-                            names.text! += (cat.value[0] as! String) + "\n"
-                            let mycount = cat.value[1] as! Int
-                            names.text! += "Count: " + String(mycount) + "\n"
-                            times += 1
-                          }
-                        names.numberOfLines = 3 * times
-                        names.frame = CGRect(x: 0, y: 0, width: 100, height: 75 * times)
-                        names.center = CGPoint(x: xcor, y: ycor + CGFloat(75 * times))
-                        names.backgroundColor = UIColor.red
-                        self.view.addSubview(names)
-                        ycor += CGFloat(150 * times)
                       }
                   }
               }
@@ -76,11 +53,38 @@ class EndorsementsViewController: UIViewController{
                     let first = data["firstname"] as? String
                     let last = data["lastname"] as? String
                     self.title = username
-                    self.displayNAME.text = username
+                    self.navBar.title = username
                     self.firstNAME.text = first
                     self.lastNAME.text = last
                 }
             }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        userEndos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let tripIndex = indexPath.row
+        cell.textLabel?.text = userEndos[tripIndex]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let mainTabController = storyboard?.instantiateViewController(identifier: "CategoryViewController") as! CategoryViewController
+        self.view.window?.rootViewController = mainTabController
+        self.view.window?.makeKeyAndVisible()
+        cat = userEndos[indexPath.row]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is CategoryViewController
+        {
+            let vc = segue.destination as? CategoryViewController
+            vc?.category = cat
+            vc?.endorsements = endoses
         }
     }
 }
